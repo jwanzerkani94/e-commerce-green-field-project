5fd813148b55ad36355e0750ec89e2a2833e43ad
areejmayadeh <engareej94@gmail.com>
2017-10-07 13:27:42 +0300

    add comments

--- Server/index.js
+++ Server/index.js
@@ -1,40 +1,34 @@
 var express=require('express');
-var morgan=require('morgan');
 var mongoose=require('mongoose');
 var models=require('../database/index');
+
+
+//Middleware
+var morgan=require('morgan');
 var bodyParser = require('body-parser');
-var bcrypt = require('bcrypt-nodejs');
-// var errorHandler = require('errorhandler')
 
-var app=express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false}));
-
-var port=process.env.PORT || 3000;
-
-
-app.use(express.static(__dirname+'/../client/'))
-app.use(morgan('dev'));
 
 var session = require('express-session');
 
+
+var app=express();
+var port=process.env.PORT || 3000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(express.static(__dirname+'/../client/')) //Serve the client files.
+app.use(morgan('dev'));
 app.use(session({
 	secret: 'shhh, it\'s a secret',
 	resave:false,
 	saveUninitialized: true
 }));
 
-var user = "";
-var createSession = function(req, res, newUser) {
-  return req.session.regenerate(function() {
-  	  user = newUser;
-      req.session.user = newUser;
-      res.redirect('/');
-    });
-};
-app.get('/login', function(req,res){
-	res.redirect('/')
-})
+
+
+//Login Function used to recieve username and password from client and search for it in database.  
+//Variable result used to manage the visibilty of login and sign up buttons , if it's true that`s mean login and signup buttons should be visible.
 var result='true';
 app.post('/login',function(req,res) {
 	var username=JSON.parse(Object.keys(req.body)[0]).username;
@@ -45,12 +39,12 @@ app.post('/login',function(req,res) {
 		}else{
 			if(!user){
 				result='true';
-				res.redirect('/');            //********************************
+				res.redirect('/');            
 			}else{
 				if(password===user.password){
 
-                   result='false';
-                 createSession(req,res,user);
+					result='false';
+					createSession(req,res,user);
 				}else{
 					res.redirect('/');
 				}
@@ -61,10 +55,12 @@ app.post('/login',function(req,res) {
 	});
 });
 
+
+
+//SignUp function used to recieve username and password from the client and add it to database as new user.
 app.post('/signup',function(req, res){
 	var username=JSON.parse(Object.keys(req.body)[0]).username;
 	var password=JSON.parse(Object.keys(req.body)[0]).password;
-	console.log(username)
 	models.User.findOne({username:username},function(err, user){
 		if(err){
 			console.log(err)
@@ -89,37 +85,62 @@ app.post('/signup',function(req, res){
 		}
 	})
 });
+
+
+//Returns the result value to client 
 app.get('/result',function (req,res) {
 	res.end(result);
-})
+});
+
+
+//Logout function destroys the open session.
 app.post('/logout',function (req,res) {
 	req.session.destroy(function() {
 		result='true';
-    res.redirect('/');
-  });
+		res.redirect('/');
+	});
 });
+
+
+
+//takes the item with it`s information and store it in the database.
 app.post('/add',function(req,res){
 	var data = JSON.parse(Object.keys(req.body)[0]);
 	var n=data.name;
 	var d=data.description;
 	var p=data.price;
 	var i=data.image;
-	console.log(user._id,data.name,data.description,data.price,data.image)
 	var newItem = {
- 		user: user._id,
- 		username:user.username,
- 		name:n,
- 		description:d,
- 		price:p,
- 		image:i
- 	}
- 	models.Item.create(newItem)
- 	res.redirect('/')
-})
+		user: user._id,
+		username:user.username,
+		name:n,
+		description:d,
+		price:p,
+		image:i
+	}
+	models.Item.create(newItem)
+	res.redirect('/')
+});
+
+//Returns all the items to client side. 
 app.get('/data',function(req,res){
 	models.Item.find({},function(err,items){
-     console.log(items);
-     res.send(items)
+		console.log(items);
+		res.send(items)
 	})
-})
+});
+
+
+
 app.listen(port)
+
+
+/////Helper function///
+var user = ""; //store the current user in it 
+var createSession = function(req, res, newUser) {
+	return req.session.regenerate(function() {
+		user = newUser;
+		req.session.user = newUser;
+		res.redirect('/');
+	});
+};
